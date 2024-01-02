fa0dfbf11117de90439b222b86f567ecf3ff4c30
HarshbirSethi <86871827+HarshbirSethi@users.noreply.github.com>
2024-01-02 08:43:56 +0000

    commit all changes

---
+++ js-training/01_variables.js
@@ -0,0 +1,10 @@
+"use strict"  // it will treat all js code as a new version
+
+const accountUsername=144556
+let accountPassword=123456
+let accountEmail="harshbir2005@gmail.com"
+let accountState="Ludhiana"
+let accountCountry
+
+console.log(accountUsername)
+console.table([accountUsername,accountPassword,accountEmail,accountCountry,accountState])
