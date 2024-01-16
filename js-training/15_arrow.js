9800c732b686b682bad0970726acfad6aa04bad5
HarshbirSethi <86871827+HarshbirSethi@users.noreply.github.com>
2024-01-16 15:35:20 +0000

    commit all changes

--- js-training/15_arrow.js
+++ js-training/15_arrow.js
@@ -18,3 +18,46 @@ const sum = (a,b) => {
     console.log(a+b);
 }
 sum(10,20)
+
+num=[2,5,7,9,3,4]
+const max = (num) => {
+    console.log(`Maximum number = ${Math.max(...num)}`)
+    console.log(`Minimum number = ${Math.min(...num)}`)
+}
+max(num)
+
+const factorial = (n) => {
+    if(n===0)
+    {
+        return 1;
+    }
+    else
+    {
+        return n*factorial(n-1)
+    }
+}
+console.log(factorial(10))
+
+const sum_num = (a) => {
+    if(a>0)
+    {
+        return a+sum_num(a-1)
+    }
+    else
+    {
+        return a
+    }
+}
+console.log(sum_num(10));
+
+const fibonacci = (n) => {
+    if(n < 2) {
+        return n;
+    }
+    else {
+        return fibonacci(n-1) + fibonacci(n - 2);
+    }
+}
+for(let i = 0; i < 10; i++) {
+    console.log(fibonacci(i));
+}
